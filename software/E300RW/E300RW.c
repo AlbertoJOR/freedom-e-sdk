14cc5cecbe8d110fe316e43089610390f99a64cd
AlbertoJ0 <84932822+AlbertoJOR@users.noreply.github.com>
2023-07-12 01:22:11 -0600

    update RW

--- software/E300RW/E300RW.c
+++ software/E300RW/E300RW.c
@@ -4,8 +4,76 @@
 
 #include <stdio.h>
 
+int main(void) {
+    static unsigned plain_text []={0x2234, 0x2678, 0x2389, 0x2992};
+    static unsigned asso_text []={0xacac, 0xfefe, 0x8181, 0x5d5d};
+    static unsigned cipher_text []= {0x0, 0x0, 0x0 , 0x0};
+    static unsigned dumm_array []= {0x0, 0x0, 0x0, 0x0};
+    unsigned plain_len = 8;
+    unsigned asso_len = 0; /// CReo que asignaste a memoria
+    unsigned plain_len_int = plain_len / 4 + 1;
 
 
+    printf("Init AEAD \n");
+    unsigned start, end, HWcycles;
+    int rd = 0;
+    // RoCC
+    start = rdcycle();
+    write_csr(mstatus, MSTATUS_XS); // Always initialize the CSR such that the accelerator is recognized
+
+    //rd = AEAD_E_Set_P( plain_text, plain_len);
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DSS(0,rd, plain_text, plain_len, Enc_Set_P );
+    asm volatile("fence" ::: "memory");
+    printf("Set Plain text finish : %d \n", rd);
+
+    //rd = AEAD_E_Set_AD(plain_text, plain_len );
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DSS(0, rd,asso_text, asso_len, Enc_Set_AD );
+    asm volatile("fence" ::: "memory");
+    printf("Set Associated Data finish : %d \n", rd);
+
+    // rd = AEAD_E_Set_C_Tag(cipher_text,dumm_array);
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DSS(0,rd, cipher_text, dumm_array, Enc_Set_C_Tag );
+    asm volatile("fence" ::: "memory");
+    printf("Set Tag and C finish : %d \n", rd);
+
+    // rd = AEAD_E_Set_Nonce(dumm_array );
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0,rd, dumm_array, Enc_Set_Nonce );
+    asm volatile("fence" ::: "memory");
+    printf("Set Nonce finish : %d \n", rd);
+
+    // rd = AEAD_E_Set_Key(rd);
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0,rd,  rd, Enc_Set_Key );
+    asm volatile("fence" ::: "memory");
+    printf("Set Key  finish : %d \n", rd);
+
+    //rd = AEAD_E_Set_Init();
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0,rd, rd,  Enc_Init );
+    asm volatile("fence" ::: "memory");
+    printf("Finish AEAD : %x \n", rd);
+
+    end = rdcycle();
+    HWcycles = end - start;
+    printf("Total time = %d cycles\n",HWcycles);
+    for(int i = 0; i < plain_len_int; i ++){
+       if(i%2==0 ){
+            printf("\n");
+        }
+        printf("%x ",cipher_text[i]);
+
+    }
+    printf("\n");
+
+
+    return 0;
+}
+
+/*
 int main(void) {
     static unsigned plain_text []={0x1234, 0x5678, 0x2389, 0x2342};
     static unsigned cipher_text []= {0x0, 0x0, 0x0 , 0x0};
@@ -22,7 +90,7 @@ int main(void) {
 
     //rd = AEAD_E_Set_P( plain_text, plain_len);
     asm volatile("fence");
-    ROCC_INSTRUCTION_DSS(0,rd, plain_text, plain_len, Enc_Set_P );
+    ROCC_INSTRUCTION_DSS(0,rd, plain_text, 0, Enc_Set_P );
     asm volatile("fence" ::: "memory");
     printf("Set Plain text finish : %d \n", rd);
 
@@ -70,4 +138,4 @@ int main(void) {
 
 
     return 0;
-}
+}*/
