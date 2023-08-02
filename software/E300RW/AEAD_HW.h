f3f12d8eff304ac3835171c6eaff1b3a3fdf1523
AlbertoJ0 <84932822+AlbertoJOR@users.noreply.github.com>
2023-08-02 11:16:09 -0600

    Dec_1000bytes

--- software/E300RW/AEAD_HW.h
+++ software/E300RW/AEAD_HW.h
@@ -4,6 +4,7 @@
 
 #ifndef FREEDOM_E_SDK_AEAD_HW_H
 #define FREEDOM_E_SDK_AEAD_HW_H
+
 #include "../RoCCcommon/rocc.h"
 #include <stdio.h>
 
@@ -14,41 +15,44 @@
 #define Enc_Set_Key 0x15
 #define Enc_Init 0x16
 #define Dec_Load_Tag 0x17
+#define Dec_Init 0x18
 
 // AEAD Encryption
 
 
 int AEAD_ENC(unsigned *ad_addr, unsigned ad_len,
-             unsigned *p_addr , unsigned p_len,
-             unsigned *c_addr, unsigned * nonce_addr,
+             unsigned *p_addr, unsigned p_len,
+             unsigned *c_addr, unsigned *nonce_addr,
              unsigned *key_addr);
 
-void printC(unsigned *arr, unsigned a_len, int cipher){
-   int len = (cipher)? a_len + 4 : a_len;
-    for (int i = 0; i <  len; i++) {
+unsigned AEAD_DEC(unsigned *ad_addr, unsigned ad_len,
+             unsigned *c_addr, unsigned c_len,
+             unsigned *d_addr, unsigned *nonce_addr,
+             unsigned *key_addr, unsigned *tag_addr);
+
+void printC(unsigned *arr, unsigned a_len, int cipher, int del) {
+    int len = (cipher) ? a_len + 4 : a_len;
+    for (int i = 0; i < len; i++) {
         if (i % 2 == 0) {
             printf("\n");
         }
         printf("%08x ", arr[i], i);
-        arr[i] = 0;
+        if (del) {
+            arr[i] = 0;
+        }
     }
 }
-
-
 int AEAD_ENC(unsigned *ad_addr, unsigned ad_len,
-             unsigned *p_addr , unsigned p_len,
-             unsigned *c_addr, unsigned * nonce_addr,
-             unsigned *key_addr){
+             unsigned *p_addr, unsigned p_len,
+             unsigned *c_addr, unsigned *nonce_addr,
+             unsigned *key_addr) {
     int rd = 0;
-      asm volatile("fence");
+    printf("Init DEC\n");
+    asm volatile("fence");
     ROCC_INSTRUCTION_DS(0, rd, nonce_addr, Enc_Set_Nonce);
     asm volatile("fence":: : "memory");
     printf("Set Nonce finish : %08x \n", rd);
 
-    /*asm volatile("fence");
-    ROCC_INSTRUCTION_DS(0, rd, Nonce, Dec_Load_Tag);
-    asm volatile("fence":: : "memory");
-    printf("Load Tag finish : %08x \n", rd);*/
 
     asm volatile("fence");
     ROCC_INSTRUCTION_DS(0, rd, key_addr, Enc_Set_Key);
@@ -66,14 +70,57 @@ int AEAD_ENC(unsigned *ad_addr, unsigned ad_len,
     printf("Set Associated Data finish length: %08x \n", rd);
 
     asm volatile("fence");
-    ROCC_INSTRUCTION_DS(0, rd, c_addr,  Enc_Set_C_Tag);
+    ROCC_INSTRUCTION_DS(0, rd, c_addr, Enc_Set_C_Tag);
     asm volatile("fence":: : "memory");
-    printf("Set Cipher addr: %08x \n", rd);
+    printf("Set Dec addr: %08x \n", rd);
 
     asm volatile("fence");
     ROCC_INSTRUCTION_DS(0, rd, rd, Enc_Init);
     asm volatile("fence":: : "memory");
-    printf("Finish AEAD : %08x \n", rd);
+    printf("Finish AEAD Dec %08x \n", rd);
+    return rd;
+}
+
+unsigned AEAD_DEC(unsigned *ad_addr, unsigned ad_len,
+             unsigned *c_addr, unsigned c_len,
+             unsigned *d_addr, unsigned *nonce_addr,
+             unsigned *key_addr, unsigned *tag_addr){
+    unsigned rd = 0;
+    printf("Init DEC\n");
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0, rd, nonce_addr, Enc_Set_Nonce);
+    asm volatile("fence":: : "memory");
+    printf("Set Nonce finish : %08x \n", rd);
+
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0, rd, tag_addr, Dec_Load_Tag);
+    asm volatile("fence":: : "memory");
+    printf("Load Tag finish : %08x \n", rd);
+
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0, rd, key_addr, Enc_Set_Key);
+    asm volatile("fence":: : "memory");
+    printf("Set Key  finish : %07x \n", rd);
+
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DSS(0, rd, c_addr, c_len, Enc_Set_P);
+    asm volatile("fence":: : "memory");
+    printf("Set Plain text finish length : %08x \n", rd);
+
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DSS(0, rd, ad_addr, ad_len, Enc_Set_AD);
+    asm volatile("fence":: : "memory");
+    printf("Set Associated Data finish length: %08x \n", rd);
+
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0, rd, d_addr, Enc_Set_C_Tag);
+    asm volatile("fence":: : "memory");
+    printf("Set Dec addr: %08x \n", rd);
+
+    asm volatile("fence");
+    ROCC_INSTRUCTION_DS(0, rd, rd, Dec_Init);
+    asm volatile("fence":: : "memory");
+    printf("Finish AEAD Dec %08x \n", rd);
     return rd;
 }
 
--- software/E300RW/E300RW.c
+++ software/E300RW/E300RW.c
@@ -5,19 +5,21 @@
 #include <stdio.h>
 
 int main(void) {
+    unsigned plain_len = 32;
+    unsigned asso_len = 32; /// CReo que asignaste a memoria
+    unsigned plain_len_int = plain_len / 4;
+    plain_len_int = (plain_len % 4 == 0) ? plain_len_int : plain_len_int + 2;
+
     static unsigned plain_text[500] = {0x11121314, 0x25262728, 0x393a3b3c, 0x0d0e0f00,
                                        0x41424344, 0x55565758, 0x696a6b6c, 0x7d7e7f70};
     static unsigned asso_text[500] = {0x91929394, 0xa5a6a7a8, 0xb9babbbc, 0xcdcecfc0,
                                       0xd1d2d3d4, 0xe5e6e7e8, 0xf9fafbfc, 0x8d8e8f80};
     static unsigned cipher_text[500 + 4] = {0};
     static unsigned Nonce[4] = {0x76777777, 0xeeeeeeee, 0xffffffff, 0x33333332};
-    // static unsigned Nonce [4]= {0};
-    static unsigned Key[4] = {0x11111111,0x22222222,0x33333333,0x44444445};
-    static unsigned dummy_array[4] = {0};
-    unsigned plain_len = 32;
-    unsigned asso_len = 32; /// CReo que asignaste a memoria
-    unsigned plain_len_int = plain_len / 4;
-    plain_len_int = (plain_len % 4 == 0) ? plain_len_int : plain_len_int + 1;
+    static unsigned dec_text[500 + 4] = {0};
+    static unsigned Key[4] = {0x11111111, 0x22222222, 0x33333333, 0x44444445};
+
+    static unsigned *tag_addr;
 
 
     // printf("Init AEAD \n");
@@ -33,26 +35,36 @@ int main(void) {
     start = rdcycle();
     write_csr(mstatus, MSTATUS_XS); // Always initialize the CSR such that the accelerator is recognized
 
-    AEAD_ENC(asso_text,asso_len,plain_text,plain_len,cipher_text,Nonce,Key);
+    AEAD_ENC(asso_text, asso_len, plain_text, plain_len, cipher_text, Nonce, Key);
 
     end = rdcycle();
     HWcycles = end - start;
     //  printf("Total time = %d cycles\n",HWcycles);
-    printC(cipher_text,plain_len_int,1);
+    printC(cipher_text, plain_len_int, 1, 1);
     printf("\nX\n");
 
 
-    AEAD_ENC(asso_text,asso_len,plain_text,plain_len,cipher_text,Nonce,Key);
-    end = rdcycle();
-    HWcycles = end - start;
-    printC(cipher_text,plain_len_int,1);
+    AEAD_ENC(asso_text, asso_len, plain_text, plain_len, cipher_text, Nonce, Key);
+    printC(cipher_text, plain_len_int, 1, 1);
     printf("\nX\n");
-   plain_len = 18;
-   asso_len = 29;
+    plain_len = 1000;
+    asso_len = 1000;
+    plain_len_int = plain_len / 4;
+    plain_len_int = (plain_len % 4 == 0) ? plain_len_int : plain_len_int + 2;
+    tag_addr = cipher_text + plain_len_int;
 
-    AEAD_ENC(asso_text,asso_len,plain_text,plain_len,cipher_text,Nonce,Key);
-    end = rdcycle();
-    HWcycles = end - start;
-    printC(cipher_text,plain_len_int,1);
+
+
+
+    AEAD_ENC(asso_text, asso_len, plain_text, plain_len, cipher_text, Nonce, Key);
+    printC(cipher_text, plain_len_int, 1, 0);
     printf("\nX\n");
+    printf("Tag \n");
+    printC(tag_addr, 4, 0, 0);
+
+    unsigned rd2 = 0;
+    rd2 = AEAD_DEC(asso_text, asso_len, cipher_text, plain_len, dec_text, Nonce, Key, tag_addr);
+    printC(dec_text, plain_len_int, 0, 0);
+    printf("\n%08x\n", rd2);
+
 }
