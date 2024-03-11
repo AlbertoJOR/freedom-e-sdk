85bf3210b54ecc4f3418fa2ef2ece54777cb68a9
AlbertoJOR <albertojortizr@gmail.com>
2024-03-10 21:00:36 -0600

    comit

---
+++ env2.sh
@@ -0,0 +1,8 @@
+# export RISCV_OPENOCD_PATH=~/GitHub/chipalliance/rocket-tools/riscv-openocd
+# export RISCV_PATH=~/GitHub/chipalliance/rocket-tools/riscv32-unknown-elf
+export RISCV_PATH=~/RISCV/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14
+
+
+export RISCV_OPENOCD_PATH=~/RISCV/riscv-openocd-0.10.0-2019.08.2-x86_64-linux-ubuntu14
+
+export PATH=$PATH:$RISCV_PATH/bin:$RISCV_OPENOCD_PATH/bin
